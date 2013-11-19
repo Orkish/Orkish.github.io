6f26876ad7b195642f4155ed2abf66d05a1524e1
Orkish <winson.yuan@hotmail.com>
2013-11-19 16:22:36 -0500

    updated bkground, css and bottom nav

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,27 +1,27 @@
 ///////// interactive image ////////////
 
-var image;
+// var image;
 
-$(function(){
-  image = $('#face');
-  $('#headshot').mousemove(function(e){
-    var x = e.pageX - this.offsetLeft;
-    var y = e.pageY - this.offsetTop;
-    $('#coords').html("X: " + x + " Y: " + y);
+// $(function(){
+//   image = $('#face');
+//   $('#headshot').mousemove(function(e){
+//     var x = e.pageX - this.offsetLeft;
+//     var y = e.pageY - this.offsetTop;
+//     $('#coords').html("X: " + x + " Y: " + y);
 
-    if(x >= 140 && x <= 267){
-      image.attr('src','stylesheets/img/head5.jpg');
-    } else if (x >= 268 && x <= 394) {
-      image.attr('src','stylesheets/img/head4.jpg');
-    } else if (x >= 395 && x <= 522) {
-      image.attr('src','stylesheets/img/head3.jpg');
-    } else if (x >= 523 && x <= 649) {
-      image.attr('src','stylesheets/img/head2.jpg');
-    } else if (x >= 650 && x <= 776) {
-      image.attr('src','stylesheets/img/head1.jpg');
-    }
-  });
-});
+//     if(x >= 140 && x <= 267){
+//       image.attr('src','stylesheets/img/head5.jpg');
+//     } else if (x >= 268 && x <= 394) {
+//       image.attr('src','stylesheets/img/head4.jpg');
+//     } else if (x >= 395 && x <= 522) {
+//       image.attr('src','stylesheets/img/head3.jpg');
+//     } else if (x >= 523 && x <= 649) {
+//       image.attr('src','stylesheets/img/head2.jpg');
+//     } else if (x >= 650 && x <= 776) {
+//       image.attr('src','stylesheets/img/head1.jpg');
+//     }
+//   });
+// });
 // 140 - 267
 // 268 - 394
 // 395 - 522
@@ -50,7 +50,7 @@ $(document).ready(function() {
 
 
 /////////////// top nav //////////////
-$(window).scroll(function() { 
+$(window).scroll(function() {
   if ($(this).scrollTop() > 600) {
     $('#top-nav').fadeIn();
   } else {
@@ -64,7 +64,7 @@ $(window).scroll(function() {
 
 $(window).scroll(function() {
   if ($(this).scrollTop() > 200) {
-    $('#banner-text').fadeOut(900);
+    $('#banner-text').fadeOut(800);
   } else {
     $('#banner-text').fadeIn(500);
   }
