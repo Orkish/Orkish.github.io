8d35d67a40ccba3f01657e540409d193290befb7
Orkish <winson.yuan@hotmail.com>
2013-12-03 16:19:11 -0500

    updated images and cleaned up codes

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -48,92 +48,18 @@ $(document).ready(function() {
     }
   });
 
-
-/////////////// top nav //////////////
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 600) {
-    $('#top-nav').fadeIn();
-  } else {
-    $('#top-nav').fadeOut();
-  }
-});
-
-
 ////////////// homebanner /////////////
 
 
 $(window).scroll(function() {
   if ($(this).scrollTop() > 200) {
-    $('#banner-text').fadeOut(800);
+    $('#banner-text').stop().fadeOut(100);
   } else {
     $('#banner-text').fadeIn(500);
   }
 });
 
 ///////////// Chart ////////////////
-console.log($("#myChart"));
-var ctx = $("#myChart").getContext("2d");
-new Chart(ctx).Doughnut(data,options);
-
-
-var data = [
-  {
-    value: 30,
-    color:"#F7464A"
-  },
-  {
-    value : 50,
-    color : "#E2EAE9"
-  },
-  {
-    value : 100,
-    color : "#D4CCC5"
-  },
-  {
-    value : 40,
-    color : "#949FB1"
-  },
-  {
-    value : 120,
-    color : "#4D5360"
-  }
-
-];
-
-Doughnut.defaults = {
-  //Boolean - Whether we should show a stroke on each segment
-  segmentShowStroke : true,
-  
-  //String - The colour of each segment stroke
-  segmentStrokeColor : "#fff",
-  
-  //Number - The width of each segment stroke
-  segmentStrokeWidth : 2,
-  
-  //The percentage of the chart that we cut out of the middle.
-  percentageInnerCutout : 50,
-  
-  //Boolean - Whether we should animate the chart 
-  animation : true,
-  
-  //Number - Amount of animation steps
-  animationSteps : 100,
-  
-  //String - Animation easing effect
-  animationEasing : "easeOutBounce",
-  
-  //Boolean - Whether we animate the rotation of the Doughnut
-  animateRotate : true,
-
-  //Boolean - Whether we animate scaling the Doughnut from the centre
-  animateScale : false,
-  
-  //Function - Will fire on animation completion.
-  onAnimationComplete : null
-};
-
-
-
 
 
 
